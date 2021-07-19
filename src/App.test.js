e78ff243b8f32f230e0747df84f30c8404dcc1bf
Rawdah <rawdah98@outlook.com>
2021-07-19 15:44:02 +0100

    done tests on button and link components

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,6 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import Button from './Button';
 
 test('Heading', () => {
   render(<App />);
@@ -13,3 +14,17 @@ test('Input Field', function(){
   expect(inputField).toBeInTheDocument()
   expect(inputField).toHaveAttribute("type", "text")
 })
+
+test ('Button', function (){
+  render (<App />)
+  const Button = shallow((<Button onClick={mockCallBack}>Saves</Button>));
+  expect(Button).toBeInTheDocument()
+
+})
+test ('link', function(){
+  render(<App/>)
+  const Link = screen.getByText(/Clear/i)
+  expect(Link).toBeInTheDocument()
+})
+
+test ('')
